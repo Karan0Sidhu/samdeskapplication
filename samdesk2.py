9f34ee5119da39ad098e7e4afbddf6723ed94482
Karan0Sidhu <karan02sidhu@gmail.com>
2025-10-07 18:37:52 -0600

    Update samdesk2.py

--- samdesk2.py
+++ samdesk2.py
@@ -9,27 +9,24 @@ for dx in range(-1, 2):
     for dy in range(-1, 2):
         if dx != 0 or dy != 0:
             dd.append((dx, dy))
-            
-# dd = [(-1, -1), (-1, 0), (-1, 1),
-#       (0, -1),           (0, 1),
-#       (1, -1), (1, 0), (1, 1)]
 
 def has_xmas(i, j):
     if not (1 <= i < n - 1 and 1 <= j < m - 1):
-        return False
+        return 0
     if lines[i][j] != "A":
-        return False
+        return 0
 
     # Check both diagonals
     diag_1 = f"{lines[i-1][j-1]}{lines[i+1][j+1]}"
     diag_2 = f"{lines[i-1][j+1]}{lines[i+1][j-1]}"
 
-    return diag_1 in ["MS", "SM"] and diag_2 in ["MS", "SM"]
+    if diag_1 in ["MS", "SM"] and diag_2 in ["MS", "SM"]:
+        return 1
+    else: return 0
 
 ans = 0
 for i in range(n):
     for j in range(m):
         ans += has_xmas(i, j)
-
 
 print(ans)
